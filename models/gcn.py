0510752d5e2e21601057299548fbfa127d3120c7
Kinsey <1587076787@qq.com>
2022-06-28 16:56:34 +0800

    Add files via upload

---
+++ models/gcn.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+from cogdl.layers import GCNLayer
+
+from cogdl.models import BaseModel
+
+
+class GCN(BaseModel):
+    r"""The GCN model from the `"Semi-Supervised Classification with Graph Convolutional Networks"
+    <https://arxiv.org/abs/1609.02907>`_ paper
+
+    Args:
+        in_features (int) : Number of input features.
+        out_features (int) : Number of classes.
+        hidden_size (int) : The dimension of node representation.
+        dropout (float) : Dropout rate for model training.
+    """
+
+    def __init__(
+        self,
+        in_feats,
+        hidden_size,
+        out_feats,
+        num_layers,
+        dropout=0.0,
+        activation="relu",
+        residual=False,
+        norm=None,
+    ):
+        super(GCN, self).__init__()
+        shapes = [in_feats] + [hidden_size] * (num_layers - 1) + [out_feats]
+        self.layers = nn.ModuleList(
+            [
+                GCNLayer(
+                    shapes[i],
+                    shapes[i + 1],
+                    dropout=dropout if i != num_layers - 1 else 0,
+                    residual=residual if i != num_layers - 1 else None,
+                    norm=norm if i != num_layers - 1 else None,
+                    activation=activation if i != num_layers - 1 else None,
+                )
+                for i in range(num_layers)
+            ]
+        )
+        self.num_layers = num_layers
+
+    def embed(self, graph):
+        graph.sym_norm()
+        h = graph.x
+        for i in range(self.num_layers - 1):
+            h = self.layers[i](graph, h)
+        return h
+
+    def forward(self, graph):
+        graph.sym_norm()
+        h = graph.x
+        for i in range(self.num_layers):
+            h = self.layers[i](graph, h)
+        return h
